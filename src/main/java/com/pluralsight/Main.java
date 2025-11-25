17cde87046797d6b55a68448e1f86dc375507716
Sthealth <AW@example.com>
2025-11-25 11:47:56 -0500

    Implemented a second version of table formatting

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -33,17 +33,26 @@ public class Main {
 
             ResultSet results = preparedStatement.executeQuery();
 
+            //Table V2 Table Header
+            System.out.println("ID  " + " Name" + " ".repeat(37) + "Price   " + "Stock");
+            System.out.println("---- " + "-".repeat(40) + " -------" + " -----");
+
             while (results.next()) {
                 int productID = results.getInt(1);
                 String productName = results.getString(2);
                 double productPrice = results.getDouble(3);
                 int productStock = results.getInt(4);
 
-                System.out.println("Product ID: " + productID);
+                //Table V1
+                /*System.out.println("Product ID: " + productID);
                 System.out.println("Name: " + productName);
                 System.out.println("Price: " + productPrice);
                 System.out.println("Stock: " + productStock);
-                System.out.println("--------------------------");
+                System.out.println("--------------------------");*/
+
+                //Table V2 Product Information
+                System.out.printf("%-4d %-40s %-7.2f %-5d\n",
+                        productID, productName, productPrice, productStock);
             }
 
             results.close();
