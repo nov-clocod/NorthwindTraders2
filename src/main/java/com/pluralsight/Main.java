5953c77f2746e6dc9ab6f67fdfcb27f3018b408e
Sthealth <AW@example.com>
2025-11-25 11:08:36 -0500

    add code to follow best practice for username/password. run query on product id, product name, product price, product stock, and display product information from database, and finally close all opened resources

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            if (args.length != 2) {
+                System.out.println("Error on required application details to run");
+                System.exit(1);
+            }
+
+            String username = args[0];
+            String password = args[1];
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/northwind",
+                    username,
+                    password
+            );
+
+            String query = """
+                    SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                    FROM products
+                    """;
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            ResultSet results = preparedStatement.executeQuery();
+
+            while (results.next()) {
+                int productID = results.getInt(1);
+                String productName = results.getString(2);
+                double productPrice = results.getDouble(3);
+                int productStock = results.getInt(4);
+
+                System.out.println("Product ID: " + productID);
+                System.out.println("Name: " + productName);
+                System.out.println("Price: " + productPrice);
+                System.out.println("Stock: " + productStock);
+                System.out.println("--------------------------");
+            }
+
+            results.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (Exception ex) {
+            System.out.println("Error occur!");
+            System.out.println(ex.getMessage());
+        }
+    }
 }
